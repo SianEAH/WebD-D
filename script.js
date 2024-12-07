efab49fca2d810460ac68e75571e94bce06a5488
SianEAH <149701097+SianEAH@users.noreply.github.com>
2024-12-07 10:14:48 +0000

    Fixed some pages, finished the homepage
    Fixed some pages, added some CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Sian*/
+function changeColour (){
+	document.getElementById("topheading").style.backgroundColor = "DarkOliveGreen";
+}
+
+/*Sian*/
+function openForm () {
+	document.getElementById("form").style.display = "block";
+}
